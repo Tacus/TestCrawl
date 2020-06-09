88d9b710697c2abae63aad3fcf1bd1da46d3dae3
zhangguobing <zhangguobing@xindong.com>
2020-06-09 20:27:07 +0800

    init

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -14,17 +14,22 @@ BOT_NAME = 'tutorial'
 SPIDER_MODULES = ['tutorial.spiders']
 NEWSPIDER_MODULE = 'tutorial.spiders'
 
-USER_AGENT_LIST={
-	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-    "Accept-Language": "zh-CN,zh;q=0.8",
+DEFAULT_REQUEST_HEADERS={
+    # "Origin":"http://baidu.com",
+	"Accept": "application/json;charset=utf-8",
+    "Accept-Language": "zh,zh-CN;q=0.9,en;q=0.8",
     "Accept-Encoding":"gzip, deflate",
     "Connection":"keep-alive",
-    "Host":"baidu.cn",
-    "Referer":"http://ris.szpl.gov.cn/bol/projectdetail.aspx",
-    "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
-    "Origin":"http://baidu.com",
-    'Upgrade-Insecure-Requests':'1',
-    'Content-Type':'application/x-www-form-urlencoded'
+    "Content-Type":"application/json;charset=utf-8",
+    "Cookie":"pgv_pvi=438138880; RK=NYggDNA8Hu; ptcz=1d39a7c8f10147401ff87a11f44ac75b1f2076cbd19e4ca20bf9c949e5b20756; pgv_pvid=8483331616; o_cookie=184624026; pac_uid=1_184624026; tvfe_boss_uuid=e553d159a511b3a4; XWINDEXGREY=0; ptui_loginuin=184624026; _ga=GA1.2.692313245.1585223620; btcu_id=876711e194caa4a523f06ffe6df4c1235e7c97c3b50cd; vc=vc-2b1c9686-70a1-43a4-b11b-ddc97770c207; vc.sig=bN5lHXB1JyTj8Fzz6ehxRxeU478NCEaqjGbomsHs0dc; vc=vc-fe3b695c-a2a4-4909-9f80-8b944cdd6026; btcu_id=0b861290-1233-4c37-b123-99cf87f05c41; gaduid=5e8587bb37849; pgv_si=s3324956672; pgv_info=ssid=s7219466864; _gid=GA1.2.1351980733.1591683200; NODINX_SESS=sIy7SHai6obHJrbhnOXT9c8nZzHtXP8i-ePiYVpbLJiL9WsK1aP6WGLz8LjwBswr; csrfToken=jAu0IDWXDf4Ikjc-8nYltxzo; token-skey=b692c56f-7761-9ce7-8d01-51b5d751271c; token-lifeTime=1591723012; _gat=1; referrer=eyJpdiI6IkJkMXRWOVgyYk45YkEzWHZCeE91TGc9PSIsInZhbHVlIjoiVUZwaEg4U0dTTmV3OENsQTBUVDRrMTdxTkFVVDF4SCtaQit2MzZoTFBPZlNGQ0V6RHhHYU5RU09Ba0tUMEZyek52TzIxT3Y1TmxuTVlKRjZUOEViN0dpTERXV0ZrQUVBVUpDZUxXWTNoeGF5TVRxTmR4MDRJM21wZGVjTDNDUXpqKzM3eUFPakZjSWVcL3dOYlpTTXFIMWxhUFU2bFFGNGI5Y0ZlOFRvd3lcLzA9IiwibWFjIjoiODg3YjIyODQ2OGE0ZjBiMGJkYThiMmYxOWJmZTE4YzUxNjBlOWE3YWVjNTA4YTcxYzFiYWU3YjE1NjM3ZDJmZCJ9; bugly_session=eyJpdiI6IlNSbEJPUk04bGJrXC9sRzlaUG1tajBnPT0iLCJ2YWx1ZSI6IndTSVZ0anZXejBuVkN3a2M4dXE5T0hxb2I3Z2NlSnFweUZ3Zys5VHlPeXY2c01LaWlHcGdEeEFKMm1rQTRJXC9mcFpnZlwvQlJoVXNpdmZoajJpcStuQ0E9PSIsIm1hYyI6ImUwMDI4MzM5YzNiOGQ2MjIyZjViMmMwNmZjNTM0ZDc1MmZhYWYwYmYyYWY1NGI0ZjQ4ZDNmZWYzZDMwODQzMTcifQ%3D%3D",
+    "Host":"bugly.qq.com",
+    "Referer": "https://bugly.qq.com/v2/crash-reporting/errors/c57b075caf?pid=1&version=1.0.62&start=0",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Mobile Safari/537.36",
+    "x-csrf-token": "jAu0IDWXDf4Ikjc-8nYltxzo",
+    "X-token": "1389971317"
 }
 
 
@@ -67,6 +72,7 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
     'tutorial.middlewares.TutorialDownloaderMiddleware': 543,
+    'tutorial.middlewares.AgentMiddleware': None,
     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None
 }
 
--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -3,24 +3,25 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    custom_settings = {
-    "DEFAULT_REQUEST_HEADERS":{
-        'User-Agent':'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Mobile Safari/537.36',
-        'x-csrf-token': 'jAu0IDWXDf4Ikjc-8nYltxzo',
-        'X-token': '1114160773',
-        }
+    # custom_settings = {
+    # "DEFAULT_REQUEST_HEADERS":{
+    #     'User-Agent':'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Mobile Safari/537.36',
+    #     'x-csrf-token': 'jAu0IDWXDf4Ikjc-8nYltxzo',
+    #     'X-token': '1114160773',
+    #     }
 
-    }
+    # }
     def start_requests(self):
         urls = [
-            'Request URL: https://bugly.qq.com/v2/issueList?start=0&searchType=errorType&exceptionTypeList=AllCatched,Unity3D,Lua,JS&pid=1&platformId=1&sortOrder=desc&version=1.0.62&rows=10&sortField=uploadTime&appId=c57b075caf&fsn=93a34258-8e97-420f-8e40-57ca46824915',
-        ]
+           'https://bugly.qq.com/v2/issueList?start=0&searchType=errorType&exceptionTypeList=AllCatched,Unity3D,Lua,JS&pid=1&platformId=1&sortOrder=desc&version=1.0.62&rows=10&sortField=uploadTime&appId=c57b075caf&fsn=2c428bf7-1e3a-4be4-8336-80082bb5aa6a'
+           ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
+        # page = response.url.split("/")[-2]
+        filename = 'bugly.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
+        print(response.body)
